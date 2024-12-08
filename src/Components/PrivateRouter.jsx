4a91c979d10f2e48b23646528056121c8f038dbe
abunaim34 <farhanadnanfarabi@gmail.com>
2024-12-08 21:44:54 +0600

    private route

--- src/Components/PrivateRouter.jsx
+++ src/Components/PrivateRouter.jsx
@@ -2,19 +2,19 @@ import { Navigate } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
-const PrivateRouter = ({children}) => {
-    const {user, loading} = useContext(AuthContext)
+const PrivateRouter = ({ children }) => {
+    const { user, loading } = useContext(AuthContext)
 
-    if(loading){
+    if (loading) {
         return <span className="loading loading-bars loading-lg flex"></span>
     }
-    if(user){
+    if (user) {
         return children;
     }
     return <Navigate to="/login"></Navigate>
 };
 
-PrivateRouter.propTypes ={
+PrivateRouter.propTypes = {
     children: PropTypes.node.isRequired
 }
 
