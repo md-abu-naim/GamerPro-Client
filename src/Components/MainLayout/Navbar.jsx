4ae16791597e5d88d7ae35bd894d199c7f30af05
abunaim34 <farhanadnanfarabi@gmail.com>
2024-12-08 21:39:37 +0600

    navber

--- src/Components/MainLayout/Navbar.jsx
+++ src/Components/MainLayout/Navbar.jsx
@@ -6,7 +6,7 @@ import { Tooltip } from 'react-tooltip'
 
 const Navbar = () => {
 
-    const { user, logOut} = useContext(AuthContext)
+    const { user, logOut } = useContext(AuthContext)
 
     const links = <>
 
