1ca437646bd6767a1563f4406837e5740f5649b0
abunaim34 <farhanadnanfarabi@gmail.com>
2024-12-08 21:41:29 +0600

    arror page

--- src/Components/Error.jsx
+++ src/Components/Error.jsx
@@ -4,12 +4,12 @@ const Error = () => {
     return (
         <div className=''>
             <div className='flex justify-center items-center p-10'>
-            <h1 className='font-bold text-6xl'>THIS PAGE IS NOT FOUND</h1>
+                <h1 className='font-bold text-6xl'>THIS PAGE IS NOT FOUND</h1>
 
             </div>
 
             <div className='flex justify-center items-center border W-[800px] h-[500px] p-9'>
-            <img src={img} alt="" />
+                <img src={img} alt="" />
             </div>
         </div>
     );
