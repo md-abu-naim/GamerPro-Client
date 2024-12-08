959783e94c32507ee133a33e39e0a70744136265
abunaim34 <farhanadnanfarabi@gmail.com>
2024-12-08 21:43:17 +0600

    high reted game

--- src/Components/HighRated.jsx
+++ src/Components/HighRated.jsx
@@ -25,41 +25,41 @@ const HighRated = () => {
 
             {
                 loading ? <div><span className="loading loading-bars loading-xs"></span>
-                <span className="loading loading-bars loading-sm"></span>
-                <span className="loading loading-bars loading-md"></span>
-                <span className="loading loading-bars loading-lg"></span></div> : 
-                <div>
-            
-                <div className='p-6 flex flex-col justify-center items-center text-center'>
-    
-                    <h1 className='font-bold text-4xl'>MOST HIGH RATER  <Typewriter words={['GAMES', 'Review', 'Code', 'Repeat!']}loop={5}></Typewriter></h1>
-                    <p className='font-bold text-2xl p-3'>Explore our collection of the most highly-rated games that have captured the hearts of gamers worldwide. Dive in and discover your next favorite! </p>
-    
-                </div>
-                <div className='grid p-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
-                    {
-                        rates.map(rate =>
-                            <div key={rate._id} className="card bg-base-100 w-96 shadow-xl">
-                                <figure>
-                                    <img
-                                        src={rate.gameCover}
-                                        alt={rate.gameTitle || "Movie"}
-                                        className="w-full h-48 object-cover"
-                                    />
-                                </figure>
-                                <div className="card-body">
-                                    <h2 className="card-title">{rate.gameTitle}</h2>
-                                    <p>{rate.reviewDescription}</p>
-                                    <div className="card-actions justify-end">
-                                        <Link to={`/details/${rate._id}`} data-tooltip-id="my-tooltip" data-tooltip-content="Explore the review" className="btn btn-outline">Explore Details</Link>
+                    <span className="loading loading-bars loading-sm"></span>
+                    <span className="loading loading-bars loading-md"></span>
+                    <span className="loading loading-bars loading-lg"></span></div> :
+                    <div>
+
+                        <div className='p-6 flex flex-col justify-center items-center text-center'>
+
+                            <h1 className='font-bold text-4xl'>MOST HIGH RATER  <Typewriter words={['GAMES', 'Review', 'Code', 'Repeat!']} loop={5}></Typewriter></h1>
+                            <p className='font-bold text-2xl p-3'>Explore our collection of the most highly-rated games that have captured the hearts of gamers worldwide. Dive in and discover your next favorite! </p>
+
+                        </div>
+                        <div className='grid p-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+                            {
+                                rates.map(rate =>
+                                    <div key={rate._id} className="card bg-base-100 w-96 shadow-xl">
+                                        <figure>
+                                            <img
+                                                src={rate.gameCover}
+                                                alt={rate.gameTitle || "Movie"}
+                                                className="w-full h-48 object-cover"
+                                            />
+                                        </figure>
+                                        <div className="card-body">
+                                            <h2 className="card-title">{rate.gameTitle}</h2>
+                                            <p>{rate.reviewDescription}</p>
+                                            <div className="card-actions justify-end">
+                                                <Link to={`/details/${rate._id}`} data-tooltip-id="my-tooltip" data-tooltip-content="Explore the review" className="btn btn-outline">Explore Details</Link>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </div>
-                        )}
-                </div>
-            </div>
+                                )}
+                        </div>
+                    </div>
             }
-             <Tooltip id="my-tooltip" />
+            <Tooltip id="my-tooltip" />
         </>
     );
 };
